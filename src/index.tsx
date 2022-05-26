962cec4be5c9950a5ba8d37aa01b8009fa9f0383
blackparadise0407 <blackparadise0407@gmail.com>
2022-05-26 23:40:45 +0700

    Refactor boilerplate code

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client'
+import React, { Suspense } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import App from 'app/App'
+import { client } from 'graphqlClient'
+
+import reportWebVitals from './reportWebVitals'
+import './i18n'
+import './index.css'
+
+const root = createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <Suspense fallback="Loading...">
+                <App />
+            </Suspense>
+        </BrowserRouter>
+    </ApolloProvider>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
