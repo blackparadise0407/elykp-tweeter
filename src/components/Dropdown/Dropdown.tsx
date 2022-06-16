6574a3ceb92ea701abb1ce49ba4d69bea8ac97a2
blackparadise0407 <blackparadise0407@gmail.com>
2022-06-16 23:33:26 +0700

    Implement dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx'
+import { AnimatePresence, motion, Variants } from 'framer-motion'
 import { CSSProperties, memo, useEffect, useRef, useState } from 'react'
 
 import { Backdrop } from 'components/Backdrop'
@@ -5,9 +7,19 @@ import { useEventListener } from 'hooks/useEventListener'
 
 import { DropdownProps } from '.'
 
+const variants: Variants = {
+    exit: {
+        opacity: 0,
+        y: 20,
+    },
+    animate: {
+        opacity: 1,
+        y: 0,
+    },
+}
+
 export default memo(function Dropdown({ children, items }: DropdownProps) {
     const wrapperRef = useRef<HTMLDivElement>(null)
-    const menuRef = useRef<HTMLDivElement>(null)
     const [open, setOpen] = useState(false)
     const [style, setStyle] = useState<CSSProperties>({
         position: 'absolute',
@@ -22,47 +34,63 @@ export default memo(function Dropdown({ children, items }: DropdownProps) {
     )
 
     useEffect(() => {
-        if (wrapperRef.current && menuRef.current) {
+        if (wrapperRef.current) {
             const { innerWidth } = window
-            const { offsetTop, offsetLeft, clientHeight } = wrapperRef.current
-            const { clientWidth } = menuRef.current
-            if (offsetLeft + clientWidth >= innerWidth) {
-                console.log('wow')
-            }
+            const { offsetTop, offsetLeft, clientHeight, clientWidth } =
+                wrapperRef.current
+
             setStyle((s) => ({
                 ...s,
-                top: `${offsetTop + clientHeight}px`,
-                left: `${offsetLeft - clientWidth}px`,
+                top: `${offsetTop + clientHeight + 10}px`,
+                right: `${innerWidth - clientWidth - offsetLeft}px`,
             }))
         }
-    }, [menuRef.current])
+    }, [])
 
     return (
         <>
-            <div ref={wrapperRef}>
+            <div
+                className={clsx(
+                    'cursor-pointer hover:bg-neutral-100 dark:hover:bg-neutral-600 p-2 rounded-lg transition-colors',
+                    open && 'bg-neutral-100 dark:bg-neutral-600',
+                )}
+                ref={wrapperRef}
+            >
                 {typeof children === 'function'
                     ? children({ isOpen: open })
                     : children}
             </div>
-            {open && (
-                <Backdrop onClick={() => setOpen(false)} transparent>
-                    <div
-                        ref={menuRef}
-                        style={style}
-                        className="bg-white dark:bg-neutral-800 py-3.5 px-3 min-w-[200px] shadow rounded-lg"
-                        onClick={(e) => e.stopPropagation()}
-                    >
-                        {items.map(({ label, key }) => (
-                            <div
-                                className="p-3 text-xs cursor-pointer hover:bg-neutral-100 dark:hover:bg-neutral-600 rounded-lg"
-                                key={key}
-                            >
-                                {label}
-                            </div>
-                        ))}
-                    </div>
-                </Backdrop>
-            )}
+            <AnimatePresence>
+                {open && (
+                    <Backdrop onClick={() => setOpen(false)} transparent>
+                        <motion.div
+                            variants={variants}
+                            initial="exit"
+                            animate="animate"
+                            exit="exit"
+                            style={style}
+                            className="bg-white dark:bg-neutral-800 py-3.5 px-3 min-w-[200px] shadow dark:shadow-neutral-900 rounded-lg"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            {items.map(({ key, icon, label, cb }) => (
+                                <div
+                                    className="flex items-center gap-2.5 p-3 text-black dark:text-white hover:bg-neutral-100 dark:hover:bg-neutral-600 transition-colors rounded-lg text-xs font-medium cursor-pointer"
+                                    key={key}
+                                    onClick={() => {
+                                        cb?.()
+                                        setOpen(false)
+                                    }}
+                                >
+                                    {icon && (
+                                        <span className="text-lg">{icon}</span>
+                                    )}
+                                    {label}
+                                </div>
+                            ))}
+                        </motion.div>
+                    </Backdrop>
+                )}
+            </AnimatePresence>
         </>
     )
 })
--- src/components/Dropdown/index.ts
+++ src/components/Dropdown/index.ts
@@ -2,10 +2,10 @@ import { ReactNode } from 'react'
 
 export interface DropdownItem {
     key: number
-    label: ReactNode
+    label?: ReactNode
     icon?: ReactNode
+    cb?: () => void
 }
-
 export interface DropdownProps {
     children: ReactNode | ((props: { isOpen: boolean }) => ReactNode)
     items: DropdownItem[]
